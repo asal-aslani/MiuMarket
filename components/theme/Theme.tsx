366577a312f06b586172456531e4fba48c5c2411
asal-aslani <asal.aslanii1383@gmail.com>
2025-01-20 15:20:01 +0330

    feat:setup mui design like typo and font
    vazir font it dosnt apply.

---
+++ components/theme/Theme.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    direction: 'rtl',
+    typography:{
+      fontFamily:"Vazirmatn",
+    },
+    
+  });
+
+  export default theme
---
+++ components/theme/Themeprovider.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { PropsWithChildren } from 'react'
+import { ThemeProvider as MuiThemeProvider } from "@mui/material";
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import theme from './Theme';
+
+
+  
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+  
+
+function Themeprovider({children}:PropsWithChildren) {
+  return (
+    <CacheProvider value={cacheRtl}>
+      <MuiThemeProvider theme={theme}>
+        {children}
+        </MuiThemeProvider>
+        </CacheProvider>
+  )
+}
+
+export default Themeprovider
